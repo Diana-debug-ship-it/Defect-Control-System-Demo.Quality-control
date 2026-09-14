6bea6c49cba2d33b130db8898d070967b6943253
Diana Radchuk <dupshur@bk.ru>
2026-09-15 02:18:53 +0300

    feat: update cors config after adding spring security

--- src/main/java/diana/dev/quality_control_service/security/SecurityConfig.java
+++ src/main/java/diana/dev/quality_control_service/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package diana.dev.quality_control_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -16,6 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/quality-control/stream").authenticated()
